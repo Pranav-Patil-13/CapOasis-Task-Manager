922e30921e1634aff649e66451aababa2a8b4dd3
Pranav-Patil-13 <107468443+Pranav-Patil-13@users.noreply.github.com>
2025-12-20 16:29:25 +0530

    Delete Task Manager directory

--- Task Manager/static/js/script.js
+++
@@ -1,9 +0,0 @@
-function showLoader() {
-    const loader = document.getElementById("globalLoader");
-    if (loader) loader.style.display = "flex";
-}
-
-function hideLoader() {
-    const loader = document.getElementById("globalLoader");
-    if (loader) loader.style.display = "none";
-}
